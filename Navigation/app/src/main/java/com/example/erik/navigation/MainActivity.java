51b92b9408a90f7e8be5b212d212bebbd60806f1
eringu <erik.ng@hotmail.com>
2017-03-31 16:05:13 +0200

    Added navigation bar

--- Navigation/app/src/main/java/com/example/erik/navigation/MainActivity.java
+++ Navigation/app/src/main/java/com/example/erik/navigation/MainActivity.java
@@ -1,4 +1,4 @@
-package se.chalmers.justintime;
+package com.example.erik.navigation;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+            this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
@@ -99,4 +99,3 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 }
-
