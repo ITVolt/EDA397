f60579d3c46b6b1d627f568590c5d4503bb59fba
ITVolt <haar@student.chalmers.se>
2017-04-02 21:46:44 +0200

    Added an activity to for a basic timer, so far only for demonstration purposes.

---
+++ JustInTime/app/src/main/java/se/chalmers/justintime/CounterActivity.java
@@ -0,0 +1,13 @@
+package se.chalmers.justintime;
+
+/**
+ * Functionality common for all activities with timers or stopwatches.
+ * Created by Patrik on 2017-04-02.
+ */
+
+public interface CounterActivity {
+    void start();
+    void pause();
+    void reset();
+    void updateTime(long ms);
+}
