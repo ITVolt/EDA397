36ea47ce9478c00553dda6b3bb0705b6b8455757
Fridgeridge <d.michaelsson@gmail.com>
2017-04-04 17:07:19 +0200

    Database refactoring

--- JustInTime/app/src/androidTest/java/se/chalmers/justintime/DatabaseHelperTest.java
+++ JustInTime/app/src/androidTest/java/se/chalmers/justintime/DatabaseHelperTest.java
@@ -1,17 +1,25 @@
 package se.chalmers.justintime;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+import android.util.Log;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.threeten.bp.LocalDateTime;
+
+import se.chalmers.justintime.database.TimerLogEntry;
 
 import static org.junit.Assert.*;
 
 /**
  * Created by David on 2017-04-04.
  */
+@RunWith(AndroidJUnit4.class)
 public class DatabaseHelperTest {
 
     private DatabaseHelper databaseHelper;
@@ -35,17 +43,19 @@ public class DatabaseHelperTest {
 
     @Test
     public void insertTimer() throws Exception {
-
+//        TimerLogEntry entry = new TimerLogEntry(1 ,1, LocalDateTime.now(), 1);
+//        databaseHelper.insertTimer(entry);
+//        TimerLogEntry timerLogEntry = databaseHelper.getEntryById(1);
+//        assertTrue(timerLogEntry.getGroupId() == 3);
     }
 
-    @Test
-    public void getData() throws Exception {
-
-    }
 
     @Test
     public void updateTimer() throws Exception {
-
+//        TimerLogEntry entry = new TimerLogEntry(1 ,1, LocalDateTime.now(), 1);
+//        databaseHelper.insertTimer(entry);
+//        entry.setGroupId(2);
+//        databaseHelper.updateTimer(entry);
     }
 
 }
--- JustInTime/app/src/main/java/se/chalmers/justintime/DatabaseHelper.java
+++ JustInTime/app/src/main/java/se/chalmers/justintime/DatabaseHelper.java
@@ -10,27 +10,22 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import org.threeten.bp.temporal.ChronoField;
+
+import se.chalmers.justintime.database.TimerLogEntry;
+
+import static se.chalmers.justintime.database.TimerLogEntry.COLUMN_NAME_DURATION;
+import static se.chalmers.justintime.database.TimerLogEntry.COLUMN_NAME_GROUPID;
+import static se.chalmers.justintime.database.TimerLogEntry.COLUMN_NAME_ID;
+import static se.chalmers.justintime.database.TimerLogEntry.COLUMN_NAME_START_TIME;
+import static se.chalmers.justintime.database.TimerLogEntry.SQL_DELETE_ENTRIES;
+import static se.chalmers.justintime.database.TimerLogEntry.TABLE_NAME;
+
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     private static final int DATABASE_VERSION = 4;
     private static final String DATABASE_NAME = "Database.db";
-
-    private static final String TABLE_NAME = "timer_data";
-
-    private static final String COLUMN_NAME_TIMERID = "timer_id";
-    private static final String COLUMN_NAME_GROUPID = "group_id";
-    private static final String COLUMN_NAME_START_TIME = "start_time";
-    private static final String COLUMN_NAME_DURATION = "duration";
-
-    private static final String SQL_CREATE_ENTRIES = " CREATE TABLE " + TABLE_NAME +
-            "(" + COLUMN_NAME_TIMERID + " INTEGER PRIMARY KEY, " +
-            COLUMN_NAME_GROUPID + " INTEGER, " +
-            COLUMN_NAME_START_TIME + " INTEGER, " +
-            COLUMN_NAME_DURATION + " INTEGER );";
-
-
-    private static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS " + TABLE_NAME;
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -39,7 +34,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(SQL_DELETE_ENTRIES);
-        db.execSQL(SQL_CREATE_ENTRIES);
+        db.execSQL(TimerLogEntry.SQL_CREATE_ENTRIES);
     }
 
     @Override
@@ -48,36 +43,35 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         onCreate(db);
     }
 
-    public boolean insertTimer(String timerId, String groupId, String startTime, String duration) {
+    public boolean insertTimer(TimerLogEntry timerLogEntry) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put(COLUMN_NAME_TIMERID, timerId);
-        contentValues.put(COLUMN_NAME_GROUPID, groupId);
-        contentValues.put(COLUMN_NAME_START_TIME, startTime);
-        contentValues.put(COLUMN_NAME_DURATION, duration);
-
-        db.insert(TABLE_NAME, null, contentValues);
-        return true;
+        contentValues.put(COLUMN_NAME_ID, timerLogEntry.getId());
+        contentValues.put(COLUMN_NAME_GROUPID, timerLogEntry.getGroupId());
+        contentValues.put(COLUMN_NAME_START_TIME, 1); //timerLogEntry.getStartTime().getLong(ChronoField.MILLI_OF_SECOND
+        contentValues.put(COLUMN_NAME_DURATION, timerLogEntry.getDuration());
+        long id = db.insert(TABLE_NAME, null, contentValues);
+        return id > -1  ;
     }
 
-    public Cursor getData(String timerId) {
+    private Cursor getData(int timerId) {
         SQLiteDatabase db = this.getReadableDatabase();
-        Cursor res = db.rawQuery("select * from " + TABLE_NAME + " where " + COLUMN_NAME_TIMERID + " =" + timerId, null);
+        Cursor res = db.rawQuery("select * from " + TABLE_NAME + " where " + COLUMN_NAME_ID + " = " + timerId , null);
         return res;
     }
 
-    /**
-     * Method for updating the status in an error report with a specific error id
-     *
-     * @param timerId unique ID for the error report which to update
-     */
-    public void updateTimer(String timerId, String startTime) {
+    public TimerLogEntry getEntryById(int timeId) {
+        return new TimerLogEntry(getData(timeId));
+    }
 
+
+    public void updateTimer(TimerLogEntry timerLogEntry) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put(COLUMN_NAME_START_TIME, startTime);
+        contentValues.put(COLUMN_NAME_START_TIME, String.valueOf(timerLogEntry.getStartTime()));
+        contentValues.put(COLUMN_NAME_DURATION, String.valueOf(timerLogEntry.getDuration()));
 
-        db.update(TABLE_NAME, contentValues, COLUMN_NAME_TIMERID + " = ? ", new String[]{timerId});
+        db.update(TABLE_NAME, contentValues, COLUMN_NAME_ID + " = ? ", new String[]{String.valueOf(timerLogEntry.getId())});
     }
 
 }
